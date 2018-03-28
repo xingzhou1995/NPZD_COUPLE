18cce4983a4c5d5f465376f243af2d88647ba4be
xingxhou1995 <robertfly800@gmail.com>
2018-03-28 17:45:18 -0400

    size change

--- PCPM_CONVEY.f90
+++ PCPM_CONVEY.f90
@@ -3,17 +3,17 @@ module PCPM_CONVEY
 implicit none
 
 
-real(kind=8),allocatable :: PCPM_N(:,:,:)
-real(kind=8),allocatable :: PCPM_P(:,:,:)
-real(kind=8),allocatable :: PCPM_Z(:,:,:)
-real(kind=8),allocatable :: PCPM_D(:,:,:)
+real(kind=8),allocatable :: PCPM_N(:,:,:,:)
+real(kind=8),allocatable :: PCPM_P(:,:,:,:)
+real(kind=8),allocatable :: PCPM_Z(:,:,:,:)
+real(kind=8),allocatable :: PCPM_D(:,:,:,:)
 
 ! forcing
-real(kind=8),allocatable :: PCPM_T(:,:,:) !temperature
-real(kind=8),allocatable :: PCPM_L(:,:,:) !light
-real(kind=8),allocatable :: PCPM_KV(:,:)     ! diffuse coefficent
+real(kind=8),allocatable :: PCPM_T(:,:,:,:) !temperature
+real(kind=8),allocatable :: PCPM_L(:,:,:,:) !light
+real(kind=8),allocatable :: PCPM_KV(:,:,:)     ! diffuse coefficent
 
 !depth
-real(kind=8),allocatable :: PCPM_DEPTH(:,:)
+real(kind=8),allocatable :: PCPM_DEPTH(:,:,:)
 
 end module
